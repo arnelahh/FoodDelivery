0178d635d8ee74a7b53446c65084b9c1f7e9dd52
arnelahh <arnela.hodzic.23@size.ba>
2025-11-29 21:48:40 +0100

    proba
    dodavanje logina

---
+++ src/main/java/unze/fooddelivery/security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package unze.fooddelivery.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/restaurants", "/restaurants/action/", "/api/**", "/css/**", "/js/**").permitAll()
+                .requestMatchers("/restaurants/**", "/meals/**", "/orders/**").authenticated().anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults()) // default login page
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/restaurants")   // gdje će otići nakon logouta
+                        .permitAll()
+                )
+                .csrf(Customizer.withDefaults()); // CSRF ostaje uključen (već imamo hidden token u formama)
+                return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users(PasswordEncoder encoder) {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("student").password(encoder.encode("student123")).roles("USER").build(),
+                User.withUsername("admin").password(encoder.encode("admin123")).roles("ADMIN").build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
